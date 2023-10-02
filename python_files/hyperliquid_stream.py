d0f8e921ac03987639be6a11e7d964bbb2a2c209
sank <sanketvaja@berkeley.edu>
2023-10-01 17:50:13 -0700

    both streams in python

---
+++ python_files/hyperliquid_stream.py
@@ -0,0 +1,67 @@
+import asyncio
+import websockets
+import json
+from datetime import datetime
+
+## BINANCE VERSION
+async def connect_to_stream():
+    # uri = "wss://fstream.binance.com/ws"  # Replace with the WebSocket stream URL
+    uri = 'wss://api.hyperliquid-testnet.xyz/ws'   
+    COIN = 'ETH'
+    
+    async with websockets.connect(uri) as websocket:
+        n = 0
+        symbol = 'ethusdt'
+        level = 20
+        speed = 0
+        # stream = "{}@depth{}@{}ms".format(symbol.lower(), level, speed)
+        s2 = "ethusdt@depth@100ms"
+        stream = [s2]
+
+        subscription_message = {
+            "method": "subscribe",
+            "subscription": { "type": "l2Book", "coin": COIN }
+            # Add any additional fields specific to your subscription message
+        }
+        
+        await websocket.send(json.dumps(subscription_message))
+        
+
+        while True:
+            message = await websocket.recv()
+            n += 1
+            print(f'Recieved message ({n}): {message}')
+        # Process the received message as per  your requirements
+#             if unsubscription_task.done():
+#                 break   
+# # Run the connection functionee
+#                 # Wait for the unsubscription task to complete before closing the connection
+            # await send_unsubscription()
+        
+        # Optionally, you can perform any cle
+        # anup or additional tasks here
+            # await websocket.close()
+
+
+
+async def send_unsubscription(websocket):
+    await asyncio.sleep(10)  # Delay before sending the unsubscription message
+
+    symbol = 'ethusdt'
+    level = 20
+    speed = 0
+    stream = "{}@depth{}@{}ms".format(symbol.lower(), level, speed)
+
+    s2 = "ethusdt@depth@100ms"
+    stream = [stream]
+    unsubscription_message = {
+        "method": "UNSUBSCRIBE",
+        "stream_name": stream  # Replace with the actual stream name
+        # Add any additional fields specific to your unsubscription message
+    }
+
+        # json_msg2 = json.dumps({"method": "UNSUBSCRIBE", "params": stream2, "id": id})
+
+    await websocket.send(json.dumps(unsubscription_message))
+
+asyncio.get_event_loop().run_until_complete(connect_to_stream())
